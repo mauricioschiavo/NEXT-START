04f8495ea265de511576df529a928239dfc188c4
mauricioschiavo <mauricioschiavo@gmail.com>
2023-08-01 22:00:45 -0300

    Initial commit

---
+++ pages/counter.tsx
@@ -0,0 +1,11 @@
+export function Counter() {
+  const value = 0;
+
+  return (
+    <>
+      <p>
+        Counter {value} <button>Adicionar</button>
+      </p>
+    </>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { Counter } from './counter';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main className={styles.main}>
+        <Counter />
+        <Counter />
+        <Counter />
+      </main>
+
+      <footer className={styles.footer}>
+        <a href="https://next.new" target="_blank" rel="noopener noreferrer">
+          Created with&nbsp;<b>next.new</b>&nbsp;⚡️
+        </a>
+      </footer>
+    </div>
+  );
+}
